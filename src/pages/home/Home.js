01f43fd94e6dfa4519da76bc84e528c7d39536f9
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-17 17:54:54 +0200

    imported header component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Navbar } from "../../components/navbar/Navbar";
+import { Header } from "../../components/header/Header";
 
 function Home() {
   return (
     <div>
-      <Navbar />
+      <Header />
     </div>
   );
 }
