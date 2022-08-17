9c53314350bdd0a367b722bf028d8850ce95344f
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-17 17:16:44 +0200

    jsx for navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Stays
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Car Rentals
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            {" "}
+            Attractions
+          </a>
+        </li>
+      </ul>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Register
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Sign In
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
