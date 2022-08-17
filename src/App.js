5e876c385c99062828f81704dbf577178b64de59
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-17 17:01:13 +0200

    imported styling file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
+import "./styles/css/style.css";
 function App() {
   return (
     <div className="App">
