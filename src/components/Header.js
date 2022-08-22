e24c2f07c932a37e9a59fd196d6c7478e1e22cb9
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-23 01:10:39 +0200

    added checking if header is on the list or home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { Navbar } from "./Navbar";
 import { Search } from "./Search";
 
-export const Header = () => {
+export const Header = ({ type }) => {
   const [show, setShow] = useState(false);
 
   return (
-    <header className="header">
+    <header className={type === "list" ? "header list-mode" : "header"}>
       <div className="container header-container">
         <div className="navbar-container">
           <div className="logo-container">
@@ -25,11 +25,15 @@ export const Header = () => {
             ></div>
           </div>
         </div>
-        <div className="header_text">
-          <h1>Find your next stay</h1>
-          <h3>Search low prices on hotels, homes and much more...</h3>
-        </div>
-        <Search />
+        {type !== "list" && (
+          <>
+            <div className="header_text">
+              <h1>Find your next stay</h1>
+              <h3>Search low prices on hotels, homes and much more...</h3>
+            </div>
+            <Search />
+          </>
+        )}
       </div>
     </header>
   );
