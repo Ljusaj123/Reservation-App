0870d551376ab919817f1b0935de7e07163cfec4
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-23 23:25:35 +0200

    created folder for all lists

---
+++ src/const/Lists.js
@@ -0,0 +1,80 @@
+export const cityList = [
+  {
+    name: "Dublin",
+    propNumber: "123",
+    imgURL: "/images/dublin.jpg",
+  },
+  {
+    name: "Berlin",
+    propNumber: "193",
+    imgURL: "/images/berlin.jpg",
+  },
+  {
+    name: "Paris",
+    propNumber: "453",
+    imgURL: "/images/paris.jpg",
+  },
+];
+
+export const propertyList = [
+  {
+    name: "Hotels",
+    propNumber: "223",
+    imgURL: "/images/hotel.jpg",
+  },
+  {
+    name: "Motels",
+    propNumber: "193",
+    imgURL: "/images/motel.jpg",
+  },
+  {
+    name: "Resots",
+    propNumber: "453",
+    imgURL: "/images/resort.jpg",
+  },
+  {
+    name: "Villas",
+    propNumber: "67",
+    imgURL: "/images/villa.jpg",
+  },
+  {
+    name: "Apartments",
+    propNumber: "453",
+    imgURL: "/images/apartment.jpg",
+  },
+];
+export const ratingList = [
+  {
+    name: "Aparthotel Stare Miasto",
+    city: "Madrid",
+    price: " Starting from 120$",
+    rating: "8.9",
+    grade: "Excelent",
+    imgURL: "/images/dublin.jpg",
+  },
+  {
+    name: "Aparthotel Stare Miasto",
+    city: "Madrid",
+    price: " Starting from 120$",
+    rating: "8.9",
+    grade: "Excelent",
+    imgURL: "/images/dublin.jpg",
+  },
+  {
+    name: "Aparthotel Stare Miasto",
+    city: "Madrid",
+    price: " Starting from 120$",
+    rating: "8.9",
+    grade: "Excelent",
+    imgURL: "/images/dublin.jpg",
+  },
+  {
+    name: "Aparthotel Stare Miasto",
+    city: "Madrid",
+    price: " Starting from 120$",
+    rating: "8.9",
+    grade: "Excelent",
+
+    imgURL: "/images/dublin.jpg",
+  },
+];
