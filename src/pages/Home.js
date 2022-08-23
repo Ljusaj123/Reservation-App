d03c04e5cbf2888b522e130968d79c11c007f84e
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-23 23:26:59 +0200

    lists as props for list components on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { Featured } from "../components/Featured";
+import { HomeRating } from "../components/HomeRating";
+import { HomeList } from "../components/HomeList";
+import { cityList, propertyList, ratingList } from "../const/Lists";
 
 function Home() {
   return (
     <div>
       <Header />
-      <main className="home-container">
-        <Featured />
+      <main className="home-container container">
+        <h2 className="home__title">Browse by city</h2>
+        <HomeList props={cityList} />
+        <h2 className="home__title ">Browse by property type</h2>
+        <HomeList props={propertyList} />
+        <h2 className="home__title ">Home guests love</h2>
+        <HomeRating props={ratingList} />
       </main>
     </div>
   );
