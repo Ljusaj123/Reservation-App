fe39b140b99144c397aaf182e908f0df24e753f5
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-24 00:08:42 +0200

    structure for email component

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const EmailList = () => {
-  return <div>EmailList</div>;
+  return (
+    <div className="email">
+      <div className="container">
+        <h2>Save time, save money!</h2>
+        <p>Sign up and we'll send the best deals to you</p>
+        <div className="email__input-container">
+          <input type="email" placeholder="Enter email" />
+          <button className="subscribe-button">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
