99e552c1ffbdcd3bb11729807b5ee93a3428fff9
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-23 11:50:19 +0200

    created featured component for home page

---
+++ src/components/Featured.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featured__item">
+        <img src="" alt="" />
+        <div className="featured__titles">
+          <h2>Dublin</h2>
+          <h3>123 properties</h3>
+        </div>
+      </div>{" "}
+      <div className="featured__item">
+        <img src="" alt="" />
+        <div className="featured__titles">
+          <h2>Austin</h2>
+          <h3>323 properties</h3>
+        </div>
+      </div>{" "}
+      <div className="featured__item">
+        <img src="" alt="" />
+        <div className="featured__titles">
+          <h2>Reno</h2>
+          <h3>533 properties</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Header } from "../components/Header";
+import { Featured } from "../components/Featured";
 
 function Home() {
   return (
     <div>
       <Header />
+      <main className="home-container">
+        <Featured />
+      </main>
     </div>
   );
 }
