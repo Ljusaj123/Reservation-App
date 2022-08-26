2391774b402262e97b7739b9ac72d2244932223e
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-26 16:18:27 +0200

    added class to the input

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -3,12 +3,16 @@ import React from "react";
 export const EmailList = () => {
   return (
     <div className="email">
-      <div className="container">
+      <div className="email-container container">
         <h2>Save time, save money!</h2>
         <p>Sign up and we'll send the best deals to you</p>
         <div className="email__input-container">
-          <input type="email" placeholder="Enter email" />
-          <button className="subscribe-button">Subscribe</button>
+          <input
+            type="email"
+            placeholder="Enter email"
+            className="input__email"
+          />
+          <button className="button__subscribe">Subscribe</button>
         </div>
       </div>
     </div>
