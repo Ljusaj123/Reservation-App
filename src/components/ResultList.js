a923f6bb9c3353780f3efbeec85374ce1220387b
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-26 17:10:45 +0200

    changed name

---
+++ src/components/ResultList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ResultCard } from "./cards/ResultCard";
+
+export const ResultList = () => {
+  return (
+    <div className="result-list">
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+    </div>
+  );
+};
