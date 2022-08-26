fba4e365d060635dc02fc387c5140f13064f4e4e
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-26 16:51:06 +0200

    changed names of the classes

--- src/components/cards/ListCard.js
+++ src/components/cards/ListCard.js
@@ -3,9 +3,9 @@ import React from "react";
 export const ListCard = ({ props }) => {
   const { name, imgURL, propNumber } = props;
   return (
-    <div className="home-list__item">
-      <img src={imgURL} alt={name} className="home-list__item-img" />
-      <div className="home-list__item-titles">
+    <div className="cards-list__item">
+      <img src={imgURL} alt={name} className="cards-list__item-img" />
+      <div className="cards-list__item-titles">
         <h3>{name}</h3>
         <p>{`${propNumber} properties`}</p>
       </div>
--- src/components/cards/RatingCard.js
+++ src/components/cards/RatingCard.js
@@ -3,13 +3,13 @@ import React from "react";
 export const RatingCard = ({ props }) => {
   const { name, imgURL, price, city, rating, grade } = props;
   return (
-    <div className="home-list__item">
-      <img src={imgURL} alt={name} className="home-list__item-img" />
-      <h3 className="home-list__item-name">{name}</h3>
-      <p className="home-list__item-city">{city}</p>
-      <p className="home-list__item-price">{price}</p>
-      <div className="home-list__item-rating">
-        <button>{rating}</button>
+    <div className="cards-list__item">
+      <img src={imgURL} alt={name} className="cards-list__item-img" />
+      <h3 className="cards-list__item-name">{name}</h3>
+      <p className="cards-list__item-city">{city}</p>
+      <p className="cards-list__item-price">{price}</p>
+      <div className="cards-list__item-rating">
+        <button className="button__rating">{rating}</button>
         <span>{grade}</span>
       </div>
     </div>
