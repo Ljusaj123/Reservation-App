f0f5031b898ad891495e7e98a42e5653a0032fa6
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-30 18:31:32 +0200

    list priperties changed

--- src/components/cards/ListCard.js
+++ src/components/cards/ListCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const ListCard = ({ props }) => {
-  const { name, imgURL, propNumber } = props;
+  const { name, count, imageUrl } = props;
   return (
     <div className="cards-list__item">
-      <img src={imgURL} alt={name} className="cards-list__item-img" />
+      <img src={imageUrl} alt={name} className="cards-list__item-img" />
       <div className="cards-list__item-titles">
         <h3>{name}</h3>
-        <p>{`${propNumber} properties`}</p>
+        <p>{`${count} properties`}</p>
       </div>
     </div>
   );
