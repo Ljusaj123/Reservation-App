0288a76bd27f7b318fe28cefeb25db510f67080d
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-30 18:30:28 +0200

    added urls and included new lists

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Header } from "../components/Header";
 import { RatingList } from "../components/RatingList";
 import { CardsList } from "../components/CardsList";
-import { cityList, propertyList, ratingList } from "../const/Lists";
+import { cityImageList, propertyImageList, ratingList } from "../const/Lists";
 import { Email } from "../components/Email";
 import { Footer } from "../components/Footer";
 
 function Home() {
+  const cityUrl =
+    "http://localhost:5000/api/v1/hotels/count/city?cities=Dublin,Berlin,Paris";
+  const propertyUrl =
+    "http://localhost:5000/api/v1/hotels/count/type?types=hotel,hostel,villa,resort,apartment";
   return (
     <div>
       <Header />
       <main className="home-container container">
         <h2 className="home__title">Browse by city</h2>
-        <CardsList props={cityList} />
+        <CardsList images={cityImageList} url={cityUrl} />
         <h2 className="home__title ">Browse by property type</h2>
-        <CardsList props={propertyList} />
+        <CardsList images={propertyImageList} url={propertyUrl} />
         <Email />
         <h2 className="home__title ">Home guests love</h2>
         <RatingList props={ratingList} />
