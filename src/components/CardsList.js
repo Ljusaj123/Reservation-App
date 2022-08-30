0c950e5a91f42f7ddf2a0fab6b197e4ce9bcfbee
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-30 18:31:09 +0200

    data fetched, made one list with pictures

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,10 +1,34 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 import { ListCard } from "./cards/ListCard";
 
-export const CardsList = ({ props }) => {
+export const CardsList = ({ images, url }) => {
+  const { data, error, loading } = useFetch(url);
+
+  if (loading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  }
+  if (error.isError) {
+    return (
+      <>
+        <p>{error.message}</p>
+      </>
+    );
+  }
+
+  const list = data.map((item, index) => {
+    return {
+      ...item,
+      imageUrl: images[index],
+    };
+  });
   return (
     <div className="cards-list">
-      {props.map((item) => {
+      {list.map((item) => {
         return <ListCard props={item} />;
       })}
     </div>
