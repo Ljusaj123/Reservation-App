45bd5fd9b43f94043b5341bd735ab0fc2c0ac512
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 00:26:27 +0200

    used properties from fetched data

--- src/components/cards/RatingCard.js
+++ src/components/cards/RatingCard.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 export const RatingCard = ({ props }) => {
-  const { name, imgURL, price, city, rating, grade } = props;
+  const { name, photos, cheapestPrice, city, rating, grade } = props;
   return (
     <div className="cards-list__item">
-      <img src={imgURL} alt={name} className="cards-list__item-img" />
+      <img src={photos[0]} alt={name} className="cards-list__item-img" />
       <h3 className="cards-list__item-name">{name}</h3>
       <p className="cards-list__item-city">{city}</p>
-      <p className="cards-list__item-price">{price}</p>
-      <div className="cards-list__item-rating">
-        <button className="button__rating">{rating}</button>
-        <span>{grade}</span>
-      </div>
+      <p className="cards-list__item-price">Starting from {cheapestPrice}$</p>
+      {rating && (
+        <div className="cards-list__item-rating">
+          <button className="button__rating">{rating}</button>
+          <span>{grade}</span>
+        </div>
+      )}
     </div>
   );
 };
