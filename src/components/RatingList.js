8c6f4a0a2fe46d703ed76b23d12e072ab509e5ac
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 00:25:33 +0200

    data fetched

--- src/components/RatingList.js
+++ src/components/RatingList.js
@@ -1,10 +1,29 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 import { RatingCard } from "./cards/RatingCard";
 
-export const RatingList = ({ props }) => {
+export const RatingList = ({ url }) => {
+  const { data, error, loading } = useFetch(url);
+  console.log(data);
+  if (loading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  }
+  if (error.isError) {
+    return (
+      <>
+        {console.log(error)}
+        <p>{error.message}</p>
+      </>
+    );
+  }
+
   return (
     <div className="cards-list">
-      {props.map((item, index) => {
+      {data.map((item, index) => {
         return <RatingCard props={item} key={index} />;
       })}
     </div>
