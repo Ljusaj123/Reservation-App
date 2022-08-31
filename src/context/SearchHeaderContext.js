47b7207e2978af36bc3ef1054c6fbb6683940c09
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 19:18:30 +0200

    created two cintexts for searching

---
+++ src/context/SearchHeaderContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const SearchHeaderContext = createContext();
+
+export default SearchHeaderContext;
+
---
+++ src/context/SearchHotelsContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const SearchHotelsContext = createContext();
+
+export default SearchHotelsContext;
