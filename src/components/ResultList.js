fe3b09199b7e963eb9d408afb28966fbe72b552e
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 02:21:19 +0200

    data fetched and added to the card component

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { ResultCard } from "./cards/ResultCard";
+import useFetch from "../hooks/useFetch";
 
-export const ResultList = () => {
+export const ResultList = ({ destination, options, date, url }) => {
+  const { data, error, loading } = useFetch(
+    `http://localhost:5000/api/v1/hotels?city=${destination}`
+  );
+  if (loading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  }
+  if (error.isError) {
+    return (
+      <>
+        <p>{error.message}</p>
+      </>
+    );
+  }
+
   return (
     <div className="result-list">
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
+      {data.map((item) => {
+        return <ResultCard props={item} key={item._id} />;
+      })}
     </div>
   );
 };
