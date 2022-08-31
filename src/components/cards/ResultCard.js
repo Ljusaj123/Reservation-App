7ed1ed97ee748de80d5ab8a6fc4715d96072c88f
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 02:21:43 +0200

    props added

--- src/components/cards/ResultCard.js
+++ src/components/cards/ResultCard.js
@@ -1,33 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const ResultCard = () => {
+export const ResultCard = ({ props }) => {
+  const { _id, name, photos, cheapestPrice, rating, distance, desc } = props;
+
   return (
     <div className="result-item">
-      <img src="/images/dublin.jpg" alt="" className="result-item__img" />
+      <img src={photos[0]} alt="" className="result-item__img" />
       <div className="result-item__desc">
-        <h3 className="result-item__title">Tower Street Apartments</h3>
-        <p className="result-item__distance">500m from center</p>
+        <h3 className="result-item__title">{name}</h3>
+        <p className="result-item__distance">${distance}m from center</p>
         <p className="result-item__taxiOp">Free airport taxi</p>
         <p className="result-item__subtitle">
           Studio Apartment with Air conditioning
         </p>
-        <span className="result-item__features">
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </span>
+        <span className="result-item__features">{desc}</span>
         <span className="result-item__cancelOp">Free cancellation </span>
         <span className="result-item__cancelOpSubtitle">
           You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className="result-item__details">
-        <div className="result-item__rating">
-          <span>Excellent</span>
-          <button className="button__rating">8.9</button>
-        </div>
+        {rating && (
+          <div className="result-item__rating">
+            <span>Excellent</span>
+            <button className="button__rating">{rating}</button>
+          </div>
+        )}
         <div className="result-item__detailTexts">
-          <h3 className="result-item__price">$112</h3>
+          <h3 className="result-item__price">{cheapestPrice}$</h3>
           <span className="result-item__taxOp">Includes taxes and fees</span>
-          <button className="button__availability">See availability</button>
+          <Link to={`${_id}`}>
+            <button className="button__availability">See availability</button>
+          </Link>
         </div>
       </div>
     </div>
