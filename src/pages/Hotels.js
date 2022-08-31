f8ed8f02cd05d3ee93b1b6352ad0b9013f4b1320
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 02:20:07 +0200

    created states for searching data

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,15 +1,46 @@
 import { Header } from "../components/Header";
-
+import { useState } from "react";
 import { ResultList } from "../components/ResultList";
 import { SearchHotels } from "../components/SearchHotels";
+import { useLocation } from "react-router-dom";
+
 function Hotels() {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [options, setOptions] = useState(location.state.options);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
+
+  const [url, setUrl] = useState("");
+
   return (
     <div>
       <Header type="list" />
       <div className="hotels-container container">
         <div className="hotels-wrapper">
-          <SearchHotels />
-          <ResultList />
+          <SearchHotels
+            destination={destination}
+            options={options}
+            date={date}
+            setDate={setDate}
+            openDate={openDate}
+            setOpenDate={setOpenDate}
+            setMin={setMin}
+            setMax={setMax}
+            min={min}
+            max={max}
+            setUrl={setUrl}
+          />
+          <ResultList
+            destination={destination}
+            options={options}
+            date={date}
+            min={min}
+            max={max}
+            url={url}
+          />
         </div>
       </div>
     </div>
