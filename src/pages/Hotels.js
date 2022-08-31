6a56156f177ecb28c7dd9ad06f5d27b0c860c658
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-31 19:24:46 +0200

    used context props, changed port and set condition to the destination

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -3,44 +3,47 @@ import { useState } from "react";
 import { ResultList } from "../components/lists/ResultList";
 import { SearchHotels } from "../components/SearchHotels";
 import { useLocation } from "react-router-dom";
+import SearchHotelsContext from "../context/SearchHotelsContext";
 
 function Hotels() {
   const location = useLocation();
-  const [destination, setDestination] = useState(location.state.destination);
+  const [destination, setDestination] = useState(
+    location.state.destination && "Split"
+  );
   const [options, setOptions] = useState(location.state.options);
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [min, setMin] = useState(undefined);
   const [max, setMax] = useState(undefined);
-
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState(
+    `http://localhost:5500/api/v1/hotels?city=${destination}`
+  );
 
   return (
     <div>
       <Header type="list" />
       <div className="hotels-container container">
         <div className="hotels-wrapper">
-          <SearchHotels
-            destination={destination}
-            options={options}
-            date={date}
-            setDate={setDate}
-            openDate={openDate}
-            setOpenDate={setOpenDate}
-            setMin={setMin}
-            setMax={setMax}
-            min={min}
-            max={max}
-            setUrl={setUrl}
-          />
-          <ResultList
-            destination={destination}
-            options={options}
-            date={date}
-            min={min}
-            max={max}
-            url={url}
-          />
+          <SearchHotelsContext.Provider
+            value={{
+              destination,
+              options,
+              date,
+              setDate,
+              openDate,
+              min,
+              max,
+              setMin,
+              setMax,
+              setOpenDate,
+              url,
+              setUrl,
+              setDestination,
+            }}
+          >
+            <SearchHotels />
+            <ResultList />
+          </SearchHotelsContext.Provider>
         </div>
       </div>
     </div>
