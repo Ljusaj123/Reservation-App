6ff34ffe6689f76e4ce3ae2774728ba47ec5c08e
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-05 23:13:39 +0200

    on click handled inline

--- src/components/cards/RatingCard.js
+++ src/components/cards/RatingCard.js
@@ -3,26 +3,22 @@ import { useNavigate } from "react-router-dom";
 
 export const RatingCard = ({ props }) => {
   const navigate = useNavigate();
-  const handleSearch = () => {
-    navigate(`/hotels/${_id}`, { state: {} });
-  };
-  const { name, photos, cheapestPrice, city, rating, _id } = props;
+
+  const { _id, name, photos, cheapestPrice, city, rating } = props;
   return (
     <div className="cards-list__item">
       <img
         src={photos[0]}
         alt={name}
         className="cards-list__item-img"
-        onClick={() => handleSearch()}
+        onClick={() => navigate(`/hotels/${_id}`)}
       />
       <h3 className="cards-list__item-name">{name}</h3>
       <p className="cards-list__item-city">{city}</p>
       <p className="cards-list__item-price">Starting from {cheapestPrice}$</p>
-      {rating && (
-        <div className="cards-list__item-rating">
-          <button className="button__rating">{rating}</button>
-        </div>
-      )}
+      <div className="cards-list__item-rating">
+        <button className="button__rating">{rating}</button>
+      </div>
     </div>
   );
 };
