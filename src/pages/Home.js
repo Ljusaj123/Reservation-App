5f546c6e4e70ccb20e4c21af703b9eac1f8980ca
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-05 23:09:51 +0200

    rating url moved, added type property to the card list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,7 @@ function Home() {
   const cityUrl =
     "http://localhost:5500/api/v1/hotels/count/city?cities=Split,Solin,Zadar";
   const propertyUrl = "http://localhost:5500/api/v1/hotels/count/type";
-  const ratingUrl =
-    "http://localhost:5500/api/v1/hotels?featured=true&limit=4&max=400";
+
   return (
     <div>
       <Header type="homepage" />
@@ -19,10 +18,14 @@ function Home() {
         <h2 className="home__title">Browse by city</h2>
         <CardsList images={cityImageList} url={cityUrl} type="cities" />
         <h2 className="home__title ">Browse by property type</h2>
-        <CardsList images={propertyImageList} url={propertyUrl} />
+        <CardsList
+          images={propertyImageList}
+          url={propertyUrl}
+          type="properties"
+        />
         <Email />
         <h2 className="home__title ">Home guests love</h2>
-        <RatingList url={ratingUrl} />
+        <RatingList />
       </main>
       <Footer />
     </div>
