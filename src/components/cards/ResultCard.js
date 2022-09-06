73b4ef9c13bc5219ae26cf9667a31c9ded11848c
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-06 16:31:14 +0200

    added check for free taxi and free cancelation

--- src/components/cards/ResultCard.js
+++ src/components/cards/ResultCard.js
@@ -2,28 +2,48 @@ import { useNavigate } from "react-router-dom";
 
 export const ResultCard = ({ props }) => {
   const navigate = useNavigate();
-  const { _id, name, photos, cheapestPrice, rating, distance, desc } = props;
+  const {
+    _id,
+    name,
+    photos,
+    cheapestPrice,
+    rating,
+    distance,
+    desc,
+    freeCancelation,
+    freeAirportTaxi,
+  } = props;
 
   return (
     <div className="result-item">
       <img src={photos[0]} alt="" className="result-item__img" />
       <div className="result-item__desc">
         <h3 className="result-item__title">{name}</h3>
-        <p className="result-item__distance">${distance}m from center</p>
-        <p className="result-item__taxiOp">Free airport taxi</p>
+        <p className="result-item__distance">{distance} from center</p>
+        {freeAirportTaxi ? (
+          <p className="result-item__taxiOp">Free airport taxi</p>
+        ) : (
+          ""
+        )}
+
         <p className="result-item__subtitle">
           Studio Apartment with Air conditioning
         </p>
         <span className="result-item__features">{desc}</span>
-        <span className="result-item__cancelOp">Free cancellation </span>
-        <span className="result-item__cancelOpSubtitle">
-          You can cancel later, so lock in this great price today!
-        </span>
+        {freeCancelation ? (
+          <>
+            <span className="result-item__cancelOp">Free cancellation </span>
+            <span className="result-item__cancelOpSubtitle">
+              You can cancel later, so lock in this great price today!
+            </span>
+          </>
+        ) : (
+          ""
+        )}
       </div>
       <div className="result-item__details">
         {rating && (
           <div className="result-item__rating">
-            <span>Excellent</span>
             <button className="button__rating">{rating}</button>
           </div>
         )}
