8aad76aa53a2065be164e72b80a80279a57959ef
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-10 00:13:50 +0200

    added text

--- src/components/modules/Footer.js
+++ src/components/modules/Footer.js
@@ -2,9 +2,16 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <p>This app is modeled after the Booking.com traveling app. </p>
+    <footer className="footer">
+      <div className="footer__container container">
+        <p>
+          This is a personal project website made for practice. This app is
+          modeled after the Booking.com traveling app. It displays all available
+          accomodations by city, type and many other information. User can see
+          more details about the accomodation and reserve a room for a certain
+          date. All pictures and informations about accomodations are used from
+          the Booking.com. It uses API that is created in Node.js and Mongo DB.
+        </p>
       </div>
     </footer>
   );
